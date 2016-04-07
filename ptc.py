dee8ae41b76a5d59172027c77957702741dc9ea5
LightningX48 <triforce314@gmail.com>
2016-04-07 19:11:16 -0400

    added initial files

---
+++ ptc.py
@@ -0,0 +1,66 @@
+import sc # file for superconcentrators
+
+def PTC(r, addend):
+    if r == 1:
+        return sc.gen(1, addend)
+    else:
+        parent1 = []
+        parent2 = []
+
+        # Adds Sources
+        for i in range (2**(r)):
+            parent1.append(-1)
+            parent2.append(-1)
+
+        # Adds 1st SC copy
+        sc1, sc2 = sc.gen(r-1, addend + len(parent1))
+        
+        for i in range(2**(r-1)):
+            sc1[i] = addend + i
+            sc2[i] = addend + i+2**(r-1);
+        
+        parent1.extend(sc1);
+        parent2.extend(sc2);
+
+        # Adds 1st PTC copy
+        ptc1, ptc2 = PTC(r-1, addend + len(parent1))
+
+        for i in range(2**(r-1)):
+            ptc1[i] = addend + len(parent1) - 2**(r-1) + i
+
+        parent1.extend(ptc1)
+        parent2.extend(ptc2)
+
+        # Adds 2nd PTC copy
+        ptc1, ptc2 = PTC(r-1, addend + len(parent1))
+
+        for i in range(2**(r-1)):
+            ptc1[i] = addend + len(parent1) - 2**(r-1) + i
+
+        parent1.extend(ptc1)
+        parent2.extend(ptc2)
+        
+        # Adds 2nd SC copy
+        sc1, sc2 = sc.gen(r-1, addend + len(parent1))
+
+        for i in range(2**(r-1)):
+            sc1[i] = addend + len(parent1) - 2**(r-1) + i
+
+        parent1.extend(sc1);
+        parent2.extend(sc2);
+
+        # Adds Sinks
+        for i in range(2**(r)):
+            parent2.append(addend + len(parent1) - 2**(r-1)+ (i % 2**(r-1)))
+        
+        for i in range(2**(r)):
+            parent1.append(addend + i)
+
+        return parent1, parent2
+
+def display():
+    a, b = PTC(3, 0)
+    for i in range(len(a)):
+        print i, a[i], b[i]
+
+display()
---
+++ sc.py
@@ -0,0 +1,42 @@
+# File that stores superconcentrator implementations
+
+def gen(r, addend):
+    return butterfly(r, addend)
+
+def butterfly(r, addend):
+    numvertices = r * 2 * (2 ** r)
+
+    parent1 = []
+    parent2 = []
+
+    for i in range (numvertices):
+        parent1.append(-1)
+        parent2.append(-1)
+        # -1 signifies the lack of a parent
+
+    # First we input the parent directly below the vertice. Note that we do
+    # not input anything for the sources because they do not have parents.
+    for vertex in range (2**r, numvertices):
+        parent1[vertex] = addend + vertex - 2**r
+
+    # Now we input the parent below and to the side of the vertice.
+    for vertex in range (2**r, numvertices):
+        column = vertex % (2**r) + 1
+        row = vertex / (2**r) + 1
+        if row >= r + 1:
+            shift = row - r
+        else:
+            shift = r - row + 2
+        shift = 2 ** (shift - 1)
+        # shift represents how far to the right or left the parent is of
+        # its child
+        if (column-1) % (2*shift) < shift:
+            # This occurs if the shift from child to parent is to the
+            # right.
+            parent2[vertex] = addend + vertex - 2**r + shift
+        else:
+            # This occurs if the shift from child to parent is to the
+            # left.
+            parent2[vertex] = addend + vertex - 2**r - shift
+
+    return parent1, parent2
