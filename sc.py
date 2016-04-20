dd8ac9e3e1de013aef0cdb8d404d41399ddc03c7
LightningX48 <triforce314@gmail.com>
2016-04-20 00:20:39 -0400

    updated sc.py with john's version, i will go in and fix ptc.py later to work with his thing

--- sc.py
+++ sc.py
@@ -7,20 +7,17 @@ def gen(r, addend):
 def butterfly(r, addend):
     numvertices = r * 2 * (2 ** r)
 
-    parent1 = []
-    parent2 = []
-
+    parents = []
     for i in range (numvertices):
-        parent1.append(-1)
-        parent2.append(-1)
+        parents.append([-1, -1])
         # -1 signifies the lack of a parent
 
     # First we input the parent directly below the vertice. Note that we do
     # not input anything for the sources because they do not have parents.
     for vertex in range (2**r, numvertices):
-        parent1[vertex] = addend + vertex - 2**r
+        parents[vertex][0] = addend + vertex - 2**r
 
-    # Now we input the parent below and to the side of the vertice.
+        # Now we input the parent below and to the side of the vertice.
     for vertex in range (2**r, numvertices):
         column = vertex % (2**r) + 1
         row = vertex / (2**r) + 1
@@ -28,16 +25,16 @@ def butterfly(r, addend):
             shift = row - r
         else:
             shift = r - row + 2
-        shift = 2 ** (shift - 1)
-        # shift represents how far to the right or left the parent is of
-        # its child
+            shift = 2 ** (shift - 1)
+            # shift represents how far to the right or left the parent is of
+            # its child
         if (column-1) % (2*shift) < shift:
             # This occurs if the shift from child to parent is to the
             # right.
-            parent2[vertex] = addend + vertex - 2**r + shift
+            parents[vertex][1] = addend + vertex - 2**r + shift
         else:
             # This occurs if the shift from child to parent is to the
             # left.
-            parent2[vertex] = addend + vertex - 2**r - shift
+            parents[vertex][1] = addend + vertex - 2**r - shift
 
-    return parent1, parent2
+    return parents
