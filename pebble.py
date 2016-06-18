0895366c77b22595801f2bec5168579fafd8a597
Albert Kwon <kwonal@mit.deu>
2016-06-18 13:50:28 -0400

    -1 => None

--- pebble.py
+++ pebble.py
@@ -10,18 +10,18 @@ class PebbleGraph:
 
     def __init__(self, r):
         self.B = ptc.PTC(r,0) # line can be changed
-        self.pebble_value = [-1]*len(self.B)
+        self.pebble_value = [None]*len(self.B)
 
     def is_pebbled(self, v):
-        if (v == -1 or self.pebble_value[v] != -1):
+        if (v is None or self.pebble_value[v] is not None):
             return True
         else:
             return False
 
     def remove_pebble(self, v):
-        if(self.is_pebbled(v) and v != -1):
+        if(self.is_pebbled(v) and v is not None):
             print "Pebble removed from node "+str(v)
-        self.pebble_value[v] = -1
+        self.pebble_value[v] = None
         # John can you add code that will actually release the stored value of this vertex from memory to free up some space?
 
     def remove_pebbles(self, S):
@@ -29,15 +29,15 @@ class PebbleGraph:
             self.remove_pebble(v)
 
     def reset(self):
-        self.pebble_value = [-1]*len(self.B)
+        self.pebble_value = [None]*len(self.B)
 
     def add_pebble(self, v):
         if (self.is_source(v)):
             if (not self.is_pebbled(v)):
                 self.pebble_value[v] = utils.secure_hash(str(v))
                 print "Pebble added to node " + str(v)
-            return
-        elif ((self.is_pebbled(self.B[v][0])) and (self.B[v][1] == -1)):
+))            return
+        elif ((self.is_pebbled(self.B[v][0])) and (self.B[v][1] is None)):
             if (not self.is_pebbled(v)):
                 self.pebble_value[v] = utils.secure_hash(str(self.pebble_value[self.B[v][0]]) + str(self.pebble_value[self.B[v][1]]))
                 print "Pebble added to node " + str(v)
@@ -50,7 +50,7 @@ class PebbleGraph:
         print "Error: attempted to pebble node " + str(v) + " without pebbling both parents"
 
     def is_source(self, v):
-        return (self.B[v][0] == -1 and self.B[v][1] == -1)
+        return (self.B[v][0] is None and self.B[v][1] is None)
 
     def get_parents(self, v):
         return self.B[v]
--- ptc.py
+++ ptc.py
@@ -8,15 +8,15 @@ def PTC(r, addend):
 
         # Adds Sources
         for i in range (2**(r)):
-            parents.append([-1, -1])
+            parents.append([None, None])
 
         # Adds 1st SC copy
         sc1 = sc.gen(r-1, addend + len(parents))
-        
+
         for i in range(2**(r-1)):
             sc1[i][0] = addend + i
             sc1[i][1] = addend + i+2**(r-1);
-        
+
         parents.extend(sc1)
 
         # Adds 1st PTC copy
@@ -34,7 +34,7 @@ def PTC(r, addend):
             ptc1[i][0] = addend + len(parents) - 2**(r-1) + i
 
         parents.extend(ptc1)
-        
+
         # Adds 2nd SC copy
         sc1 = sc.gen(r-1, addend + len(parents))
 
@@ -43,11 +43,11 @@ def PTC(r, addend):
 
         parents.extend(sc1)
         sofar = len(parents)
-        
+
         # Adds Sinks
         for i in range (2**(r)):
-            parents.append([-1, -1])
-        
+            parents.append([None, None])
+
         for i in range(2**(r)):
             parents[i+sofar][0] = addend + sofar - 2**(r-1)+ (i % 2**(r-1))
             parents[i+sofar][1] = addend + i
